db3ab76c440b1a1af761bc7f53d1e06feb861e2a
Cybercrimjawir <mwiraa15@gmail.com>
2026-08-04 13:04:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let kesempatan = 5;
+let angkaRahasia = Math.floor(Math.random() * 100) + 1;
+
+function tebak() {
+    let jawaban = Number(document.getElementById("jawaban").value);
+    let hasil = document.getElementById("hasil");
+
+vif (jawaban === 67) {
+    window.location.href = "rahasia.html";
+    return;
+}
+
+    if (jawaban === angkaRahasia) {
+        hasil.innerHTML = "👑 Good job! Tebakan benar!";
+        return;
+    }
+
+    kesempatan--;
+
+    if (jawaban < angkaRahasia) {
+        hasil.innerHTML = "📈 Terlalu kecil!";
+    } else {
+        hasil.innerHTML = "📉 Terlalu besar!";
+    }
+
+    document.getElementById("kesempatan").innerHTML =
+        "❤️ Kesempatan: " + kesempatan;
+
+    if (kesempatan === 0) {
+        hasil.innerHTML = "💀 Game Over! Angka: " + angkaRahasia;
+    }
+}
+g
