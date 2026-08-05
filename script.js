adff2962ffb22b593b7cb8cb4846ac2f9cd549c6
Cybercrimjawir <mwiraa15@gmail.com>
2026-08-05 09:57:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function tebak() {
     let jawaban = Number(document.getElementById("jawaban").value);
     let hasil = document.getElementById("hasil");
 
-    if (jawaban === 67) {
+    if (jawaban === 01010111 01100101 01100010 00100000 01101001 01101110 01101001 00100000 01100010 01110101 01100001 01110100 01100001 01101110 00100000 01110111 01101001 01110010 01100001) {
     window.location.href = "rahasia.html";
     return;
 }
